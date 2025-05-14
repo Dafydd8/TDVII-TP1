b8e84f30ae13ba767d6d8881039cb86399c935bc
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-05-14 16:08:24 -0300

    first commit

---
+++ indicadores_salud_tables.sql
@@ -0,0 +1,83 @@
+CREATE TABLE Provincia (
+    id_prov SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    poblacion_total INTEGER NOT NULL
+);
+
+CREATE TABLE Persona (
+    id_persona SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+	apellido VARCHAR(100) NOT NULL,
+	segundo_nombre VARCHAR(100),
+	segundo_apellido VARCHAR(100),
+	tipo_documento VARCHAR(10) NOT NULL,
+	numero_documento VARCHAR(20) UNIQUE,
+	sexo CHAR(1) CHECK (sexo IN ('M', 'F', 'X')),
+	fecha_nacimiento DATE,
+	id_provincia INT NOT NULL REFERENCES Provincia(id_prov),
+    cobertura_medica VARCHAR(5) CHECK (cobertura_medica IN ('CPE', 'Mixta')) 
+);
+
+CREATE TABLE EfectorDeSalud (
+    cod_refes SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+	id_prov INT NOT NULL REFERENCES Provincia(id_prov),
+	nivel VARCHAR(20) CHECK (nivel IN ('Primer Nivel', 'Segundo Nivel', 'Tercer Nivel')) 
+);
+
+CREATE TABLE EfectorTercerNivel (
+	cod_refes INTEGER PRIMARY KEY,
+    tipo VARCHAR(50) CHECK (tipo IN ('Alta Tecnología', 'Instituto Especializado')),
+    FOREIGN KEY (cod_refes) REFERENCES EfectorDeSalud(cod_refes)
+);
+
+CREATE TABLE Servicio (
+    cod_servicio SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE EfectorDeSaludServicio (
+	cod_refes INT REFERENCES EfectorDeSalud(cod_refes),
+    cod_servicio INT REFERENCES Servicio(cod_servicio),
+    PRIMARY KEY (cod_refes, cod_servicio)
+);
+
+CREATE TABLE HCE (
+	id_HCE SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	id_prov INT NOT NULL REFERENCES Provincia(id_prov),
+	tipo_gestion VARCHAR(10) CHECK (tipo_gestion in ('Provincial', 'Proveedor')),
+	tipo_desarrollo VARCHAR(10) CHECK (tipo_desarrollo in ('Propio', 'Licencia')),
+	gestion_estudios VARCHAR(15) CHECK (gestion_estudios in ('Gestiona', 'No gestiona')),
+	generacion_receta_digital VARCHAR(10) CHECK (generacion_receta_digital in ('Genera', 'No genera'))
+);
+
+CREATE TABLE HCEGeneraReceta (
+	id_HCE int PRIMARY KEY,
+	farmacias_asociadas VARCHAR(10) CHECK (farmacias_asociadas IN ('Públicas', 'Todas')),
+	FOREIGN KEY (id_HCE) REFERENCES HCE(id_HCE)
+);
+
+CREATE TABLE HCEPrepagas (
+	id_HCE int PRIMARY KEY,
+	FOREIGN KEY (id_HCE) REFERENCES HCEGeneraReceta(id_HCE)
+);
+
+CREATE TABLE HCEObrasSociales (
+	id_HCE int PRIMARY KEY,
+	FOREIGN KEY (id_HCE) REFERENCES HCEGeneraReceta(id_HCE)
+);
+
+CREATE TABLE Capacidad (
+	cod_capacidad SERIAL PRIMARY KEY,
+	nombre varchar(100) NOT NULL,
+	descripcion TEXT
+);
+
+CREATE TABLE HCECapacidad (
+	id_HCE INT REFERENCES HCE(id_HCE),
+    cod_capacidad INT REFERENCES Capacidad(cod_capacidad),
+    PRIMARY KEY (id_HCE, cod_capacidad)
+);
+
+
